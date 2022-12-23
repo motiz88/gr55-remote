e749417f8c2a8be84fd187391e99830d0e2d7a39
Moti Zilberman <moti@meta.com>
2022-12-23 12:47:26 +0000

    [repo] Install eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "universe/native"],
+  extends: [
+    "eslint:recommended",
+    "universe/native",
+    "plugin:react-hooks/recommended",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
   rules: {},
 };
