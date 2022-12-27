cb2a1daa1f2b80da78662eba41e580c3a1feb13c
Moti Zilberman <moti@meta.com>
2022-12-27 02:17:40 +0000

    Support rendering numeric fields in Picker

--- PatchFieldPicker.tsx
+++ PatchFieldPicker.tsx
@@ -4,15 +4,24 @@ import { Text, View } from "react-native";
 import { PatchFieldSegmentedPicker } from "./PatchFieldSegmentedPicker";
 import { PatchFieldStyles } from "./PatchFieldStyles";
 import { Picker } from "./Picker";
-import { EnumField, FieldReference } from "./RolandAddressMap";
+import {
+  EnumField,
+  FieldReference,
+  FieldType,
+  isEnumFieldReference,
+  isNumericFieldReference,
+  NumericField,
+} from "./RolandAddressMap";
 import { useMaybeControlledPatchField } from "./usePatchField";
 
-export function PatchFieldPicker<T extends string>({
+export function PatchFieldPicker<T extends number | string>({
   field,
   value: valueProp,
   onValueChange: onValueChangeProp,
 }: {
-  field: FieldReference<EnumField<{ [encoded: number]: T }>>;
+  field: FieldReference<
+    FieldType<T> & (EnumField<{ [encoded: number]: string }> | NumericField)
+  >;
   value?: T;
   onValueChange?: (value: T) => void;
 }) {
@@ -21,20 +30,46 @@ export function PatchFieldPicker<T extends string>({
     valueProp,
     onValueChangeProp
   );
-  const items = useMemo(
-    () =>
-      Object.entries(field.definition.type.labels).map(([encoded, label]) => (
-        <Picker.Item label={label} key={encoded} value={label} />
-      )),
-    [field]
-  );
-  // TODO: Use a layout-sensitive threshold
-  if (Object.keys(field.definition.type.labels).length <= 3) {
+  const items = useMemo(() => {
+    if (isEnumFieldReference(field)) {
+      return Object.entries(field.definition.type.labels).map(
+        ([encoded, label]) => (
+          <Picker.Item label={label} key={encoded} value={label} />
+        )
+      );
+    }
+    if (!isNumericFieldReference(field)) {
+      throw new Error("Expected enum or numeric field");
+    }
+    const items = [];
+    for (
+      let i = field.definition.type.min;
+      i <= field.definition.type.max;
+      i += field.definition.type.step
+    ) {
+      items.push(
+        <Picker.Item
+          label={field.definition.type.format(i)}
+          key={i}
+          value={i}
+        />
+      );
+    }
+    return items;
+  }, [field]);
+  if (
+    // TODO: Support SegmentedPicker for numeric fields
+    isEnumFieldReference(field) &&
+    // TODO: Use a layout-sensitive threshold
+    Object.keys(field.definition.type.labels).length <= 3
+  ) {
     return (
       <PatchFieldSegmentedPicker
         field={field}
-        value={value}
-        onValueChange={onValueChange}
+        value={value as string}
+        onValueChange={
+          onValueChange as ((newValue: string) => void) | undefined
+        }
       />
     );
   }
