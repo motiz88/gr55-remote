721220ac08710d491d140c0609d8ba70e24159a2
Moti Zilberman <moti@fb.com>
2023-04-10 00:04:15 +0100

    Library: System patch names, 2D grid rendering

--- LibraryPatchListScreen.tsx
+++ LibraryPatchListScreen.tsx
@@ -1,5 +1,6 @@
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import { StyleSheet, FlatList } from "react-native";
+import { useMemo } from "react";
+import { StyleSheet, FlatList, useWindowDimensions, View } from "react-native";
 
 import {
   AsciiStringField,
@@ -14,37 +15,75 @@ import { pack7 } from "./RolandSysExProtocol";
 import { useMainScrollViewSafeAreaStyle } from "./SafeAreaUtils";
 import { ThemedText as Text } from "./ThemedText";
 import { RootTabParamList } from "./navigation";
+import { useLayout } from "./useLayout";
 import { usePatchMap } from "./usePatchMap";
 import { useRolandRemotePageState } from "./useRolandRemotePageState";
 
-const ITEM_HEIGHT = 44;
+const ITEM_HEIGHT = 2 * 32;
+const ITEM_WIDTH = 16 * 16;
+const ITEM_HPADDING = 8;
+const ITEM_VPADDING = 8;
+const ITEM_FONT_SIZE = 20;
 
 export function LibraryPatchListScreen({
   navigation,
 }: BottomTabScreenProps<RootTabParamList, "LibraryPatchList", "RootTab">) {
   const safeAreaStyle = useMainScrollViewSafeAreaStyle();
-
+  const { onLayout, ...layout } = useLayout();
+  const windowDimensions = useWindowDimensions();
+  const availableWidth = layout.width || windowDimensions.width;
+  const itemsPerRow = Math.max(
+    Math.floor(availableWidth / (ITEM_WIDTH + ITEM_HPADDING * 2)),
+    1
+  );
   const patchMap = usePatchMap();
+  const rows = useMemo(() => {
+    if (!patchMap) {
+      return [];
+    }
+    const rowsMut: RolandGR55PatchMap["patchList"][] = [];
+    for (let i = 0; i < patchMap.patchList.length; i += itemsPerRow) {
+      rowsMut.push(patchMap.patchList.slice(i, i + itemsPerRow));
+    }
+    return rowsMut;
+  }, [itemsPerRow, patchMap]);
+
   if (!patchMap) {
     return <RolandGR55NotConnectedView navigation={navigation} />;
   }
+
   return (
     <FlatList
+      onLayout={onLayout}
       style={styles.container}
       contentContainerStyle={safeAreaStyle}
-      data={patchMap.patchList}
-      renderItem={({ item }) => <PatchItem patch={item} />}
-      getItemLayout={getItemLayout}
+      data={rows}
+      renderItem={({ item }) => <PatchRow items={item} />}
+      getItemLayout={getRowLayout}
     />
   );
 }
 
-function getItemLayout(_: any, index: number) {
+function getRowLayout(_: any, index: number) {
   return {
-    length: ITEM_HEIGHT,
-    offset: ITEM_HEIGHT * index,
+    length: ITEM_HEIGHT + ITEM_VPADDING * 2,
+    offset: (ITEM_HEIGHT + ITEM_VPADDING * 2) * index,
     index,
   };
+}
+
+function PatchRow({
+  items,
+}: {
+  items: readonly RolandGR55PatchMap["patchList"][number][];
+}) {
+  return (
+    <View style={styles.row}>
+      {items.map((item) => (
+        <PatchItem patch={item} key={item.styleLabel + item.patchNumberLabel} />
+      ))}
+    </View>
+  );
 }
 
 function PatchItem({
@@ -55,10 +94,14 @@ function PatchItem({
   const patchName =
     patch.userPatch != null ? (
       <UserPatchName patch={patch} userPatch={patch.userPatch} />
-    ) : /* TODO: system patch names */ null;
+    ) : (
+      patch.builtInName
+    );
   return (
     <Text style={styles.item}>
-      {patch.styleLabel} {patch.patchNumberLabel} {patchName}
+      {patch.styleLabel} {patch.patchNumberLabel}
+      {"\n"}
+      {patchName}
     </Text>
   );
 }
@@ -108,7 +151,17 @@ const styles = StyleSheet.create({
   container: {
     padding: 8,
   },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
   item: {
-    height: ITEM_HEIGHT,
+    height: ITEM_HEIGHT + ITEM_VPADDING * 2,
+    width: ITEM_WIDTH + ITEM_HPADDING * 2,
+    paddingHorizontal: ITEM_HPADDING,
+    paddingVertical: ITEM_VPADDING,
+    fontSize: ITEM_FONT_SIZE,
+    textAlign: "center",
+    textAlignVertical: "center",
   },
 });
