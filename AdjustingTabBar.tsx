067592b05d79600bd9ac167a367a55fbc2dbb031
Moti Zilberman <moti@fb.com>
2023-11-26 15:32:20 +0000

    Work around tab bar jank on Android

--- AdjustingTabBar.tsx
+++ AdjustingTabBar.tsx
@@ -1,5 +1,5 @@
 import { MaterialTopTabBar } from "@react-navigation/material-top-tabs";
-import { Platform, StyleProp, ViewStyle } from "react-native";
+import { Dimensions, Platform, StyleProp, ViewStyle } from "react-native";
 
 export const renderAdjustingMaterialTopTabBar =
   createAdjustingTabBar(MaterialTopTabBar);
@@ -24,12 +24,16 @@ function createAdjustingTabBar<
 >(TabBar: React.ComponentType<TabBarProps>) {
   // NOTE: Not actually a component (because react-navigation calls it as a function)
   return function renderAdjustingTabBar(props: TabBarProps) {
+    // The layout from react-navigation can take multiple seconds to reach the
+    // correct value, so we use the window dimensions as a hacky workaround.
+    const effectiveLayoutWidth =
+      Dimensions.get("window").width ?? props.layout.width;
     const MINIMUM_TAB_WIDTH = Platform.select({
       ios: 75,
       default: 65,
     });
     const tabCount = Object.keys(props.descriptors).length;
-    let tabsInView = Math.floor(props.layout.width / MINIMUM_TAB_WIDTH);
+    let tabsInView = Math.floor(effectiveLayoutWidth / MINIMUM_TAB_WIDTH);
     if (tabsInView < tabCount && tabsInView > 1) {
       // round down to the nearest half tab
       tabsInView -= 0.5;
@@ -44,12 +48,19 @@ function createAdjustingTabBar<
             tabBarScrollEnabled: tabsInView < tabCount,
             tabBarItemStyle:
               tabsInView < tabCount && tabsInView > 0
-                ? { width: props.layout.width / tabsInView }
+                ? { width: effectiveLayoutWidth / tabsInView }
                 : {},
           },
         },
       ])
     );
-    return <TabBar {...props} descriptors={descriptors} />;
+    return (
+      <TabBar
+        // On Android, TabBar doesn't rerender correctly at different sizes if we don't remount it
+        key={effectiveLayoutWidth + "_" + tabsInView + "_" + tabCount}
+        {...props}
+        descriptors={descriptors}
+      />
+    );
   };
 }
